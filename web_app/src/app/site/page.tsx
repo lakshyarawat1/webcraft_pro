80fd0f89c0aadafe643ef4169e221c26fce7c524
lakshyarawat1 <lakshya.paramount@gmail.com>
2024-02-23 21:01:34 +0530

    Created landing page and added themes

--- web_app/src/app/site/page.tsx
+++ web_app/src/app/site/page.tsx
@@ -1,9 +1,95 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { pricingCards } from "@/lib/constants";
+import clsx from "clsx";
+import { Check } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Page
-    </main>
+    <>
+      <section className="h-full w-full md:pt-44 md:mt-0 -mt-44 relative flex items-center justify-center flex-col">
+        {/* grid */}
+        <div className="opacity-30 absolute h-[50rem] bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
+        <p>Create your agency, here.</p>
+        <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
+          <h1 className="text-7xl font-bold text-center md:text-[200px]">
+            {" "}
+            Webcraft
+          </h1>
+        </div>
+        <div className="flex justify-center items-center relative md:mt-[-70px]">
+          <Image
+            src={"/preview.png"}
+            alt="banner"
+            height={1200}
+            width={1200}
+            className="rounded-tl-2xl rounded-tr-2xl border-2 border-muted"
+          />
+          <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10"></div>
+        </div>
+      </section>
+      <section className="flex items-center flex-col justify-center gap-4 md:!mt-24  mt-[-100px]">
+        <h2 className="text-4xl md:mt-20 text-center">
+          Choose what fits you right
+        </h2>
+        <p className="text-muted-foreground text-center">
+          Our straightforward pricing plans are tailored to fit your needs. We{" "}
+          <br />
+          offer a variety of options for both small and large teams.
+        </p>
+        <div className="flex justify-center gap-4 flex-wrap mt-6">
+          {pricingCards.map((card, index) => (
+            <Card
+              key={index}
+              className={clsx("w-[300px] flex flex-col justify-between", {
+                "border-2 border-primary": card.title === "Unlimited Saas",
+              })}
+            >
+              <CardHeader>
+                <CardTitle
+                  className={clsx("", {
+                    "text-muted-foreground": card.title != "Unlimited Saas",
+                  })}
+                >
+                  {card.title}
+                </CardTitle>
+                <CardDescription>{card.description}</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <span className="text-4xl font-bold">{card.price}</span>
+                <span className="text-muted-foreground">/m</span>
+              </CardContent>
+              <CardFooter className="flex flex-col items-start gap-4">
+                <div>
+                  {card.features.map((feature) => (
+                    <div key={feature} className="flex gap-2 items-center">
+                      <Check className="text-muted-foreground" />
+                      <p>{feature}</p>
+                    </div>
+                  ))}
+                </div>
+                <Link
+                  href={`/agency?plan=${card.priceId}`}
+                  className={clsx(
+                    "w-full text-center bg-primary p-2 rounded-md",
+                    { "!bg-muted-foreground": card.title !== "Unlimited Saas" }
+                  )}
+                >
+                  Get Started
+                </Link>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+      </section>
+    </>
   );
 }
---
+++ web_app/src/components/site/index.tsx
@@ -0,0 +1,41 @@
+import { UserButton } from "@clerk/nextjs";
+import { User } from "@clerk/nextjs/server";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { ModeToggle } from "../global/mode-toggle";
+
+type Props = {
+  user?: null | User;
+};
+
+const Navigation = ({ user }: Props) => {
+  return (
+    <div className="p-4 flex items-center justify-between relative ">
+      <aside className="flex items-center gap-2">
+        <Image src={"plura-logo.svg"} alt="logo" width={40} height={40} />{" "}
+        <span className="text-xl font-bold">Webcraft.</span>
+      </aside>
+      <nav className="hidden md:block absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%]">
+        <ul className="flex items-center justify-center gap-6">
+          <Link href={"#"}>Pricing</Link>
+          <Link href={"#"}>About</Link>
+          <Link href={"#"}>Features</Link>
+          <Link href={"#"}>Documentation</Link>
+        </ul>
+      </nav>
+      <aside className="flex gap-2 items-center">
+        <Link
+          href={"/agency"}
+          className="bg-primary text-white p-2 px-4 hover:bg-primary/80"
+        >
+          Login
+        </Link>
+        <UserButton />
+        <ModeToggle />
+      </aside>
+    </div>
+  );
+};
+
+export default Navigation;
