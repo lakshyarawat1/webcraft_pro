0208f90fc1654e61bb310fade1c2f10d271e61cf
lakshyarawat1 <lakshya.paramount@gmail.com>
2024-02-24 12:30:37 +0530

    added middleware logic

--- web_app/src/components/site/index.tsx
+++ web_app/src/components/site/index.tsx
@@ -26,7 +26,7 @@ const Navigation = ({ user }: Props) => {
       </nav>
       <aside className="flex gap-2 items-center">
         <Link
-          href={"/agency"}
+          href={"/agency/sign-in"}
           className="bg-primary text-white p-2 px-4 hover:bg-primary/80"
         >
           Login
