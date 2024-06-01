4ba2a81a9774326fc622214f1b1a46d0e7636d01
lakshyarawat1 <lakshya.paramount@gmail.com>
2024-06-01 12:04:37 +0530

    created infobar

--- web_app/src/app/(main)/agency/[agencyId]/layout.tsx
+++ web_app/src/app/(main)/agency/[agencyId]/layout.tsx
@@ -1,3 +1,5 @@
+import BlurPage from "@/components/global/BlurPage";
+import Infobar from "@/components/global/Infobar";
 import Sidebar from "@/components/sidebar";
 import Unauthorized from "@/components/unauthorized";
 import {
@@ -35,7 +37,15 @@ const layout = async ({ children, params }: Props) => {
   return (
     <div className="h-screen overflow-hidden">
       <Sidebar id={params.agencyId} type="agency" />
-      <div className="md:pl-[300px]">{children}</div>
+      <div className="md:pl-[300px]">
+        <Infobar
+          notifications={allNotifications}
+          role={allNotifications.User?.role}
+        />
+        <div className="relative">
+          <BlurPage>{children}</BlurPage>
+        </div>
+      </div>
     </div>
   );
 };
